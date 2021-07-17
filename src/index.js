5b66ab624342774e5e5c1d7d0a9012afb6335a04
Ryochi <ryo1.t.zubu@gmail.com>
2021-07-17 10:28:38 +1000

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const taskForm = document.querySelector("#create-task-form");
   const taskList = document.querySelector("#tasks");
-  const li = document.createElement("li");
   
   //A method that takes input value as a parameter and insert the value into a list.
-  function createLists(task){
-    li.innerHTML += `${task} <button data-action="delete"> X </delete>`;
+  function createLists(task,dueDate){
+    const li = document.createElement("li");
+    li.innerHTML += `${task} ${dueDate} <button data-description="delete"> X </delete>`;
     taskList.appendChild(li);
   }
 
@@ -38,28 +38,45 @@ document.addEventListener("DOMContentLoaded", () => {
         break;
     }
   }
+
   const taskEntry = document.querySelector("#new-task-description");
   taskForm.insertBefore(select, taskEntry.nextSibling);
   //Change list color by priority
   function changeListColor(chosenPriority){
+    const li = taskList.lastElementChild;
     switch(chosenPriority){
-      case "High": li.style.backgroundColor = "red";
+      case "High": 
+        li.style.backgroundColor = "red";
+        li.setAttribute("id", "3");
         break;
-      case "Middle": li.style.backgroundColor = "yellow";
+      case "Middle": 
+        li.style.backgroundColor = "yellow";
+        li.setAttribute("id", "2");
         break;
-      case "Low": li.style.backgroundColor = "green";
+      case "Low": 
+        li.style.backgroundColor = "green";
+        li.setAttribute("id", "1");
         break;
-      default: li.style.backgroundColor = "orange";
+      default: 
+        li.style.backgroundColor = "blue";
         break;
     }
   }
+
+  //Challenge3: Due date picker
+  const date = document.createElement("input");
+  date.setAttribute("type", "date");
+  date.setAttribute("id", "dueDate");
+  date.setAttribute("value","dd/mm/yyyy");
+  taskForm.insertBefore(date, taskEntry.nextSibling);
 
   //Add a eventLisner to the form (not submit btn), when the form is submitted, prevent from posting request and get input value from textInput and pass it to createList.
   taskForm.addEventListener("submit", function(e){
     e.preventDefault();
     const inputValue = document.querySelector("#new-task-description").value;
     const selectedPriority = document.querySelector("select").value;
-    createLists(inputValue);
+    const dueDate = document.getElementById("dueDate").value;
+    createLists(inputValue,dueDate);
     changeListColor(selectedPriority);
     taskForm.reset();
   });
@@ -67,8 +84,10 @@ document.addEventListener("DOMContentLoaded", () => {
   //Challenge1: A delete function
   //Add a eventListner to the ul list and check if dataset is equal to delete and delete the li tag itself.
   taskList.addEventListener("click", function(e){
-    if(e.target.dataset.action === "delete"){
+    if(e.target.dataset.description === "delete"){
       e.target.parentElement.remove();
     }
   });
+
+  
 });
